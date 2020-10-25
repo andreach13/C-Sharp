31deb6a138bf64d4e4a7d923bb8d5b2bc8e8ce78
Andrea Chavarria <andrea.chavarria@unah.hn>
2020-10-25 12:36:23 -0600

    agregando tarea 4-II unidad

---
+++ Ejercicios/7-Tarea-Inventario POO/inventario-poo/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace inventario_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion =0;
+            string razon = "";
+
+            Inventario inventario = new Inventario();
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Sistema de Inventario");
+                Console.WriteLine("***********************");
+                Console.WriteLine();
+                Console.WriteLine ("1 - Productos");
+                Console.WriteLine ("2 - Ingreso de Inventario");
+                Console.WriteLine ("3 - Salida de Inventario");
+                Console.WriteLine ("4 - Ajuste Positivo de Inventario");
+                Console.WriteLine ("5 - Ajuste Negativo de Inventario");
+                Console.WriteLine ("0 - Salir");
+                Console.WriteLine("");
+                string linea =Console.ReadLine();
+                opcion = int.Parse(linea);
+
+                switch (opcion)
+                {
+                    case 1:                    
+                        inventario.listarProductos();
+                        break;
+                    case 2:    
+                        inventario.ingresoDeInventario();
+                        break; 
+                    case 3:
+                        inventario.salidaDeInventario();
+                        break;
+                    case 4:
+                        inventario.ajustePositivoDeInventario();
+                        if (opcion == 4)
+                        {
+                            Console.Write("Especifique la razon del ajuste: ");
+                            razon = Console.ReadLine();
+                        }
+                            if (string.IsNullOrEmpty(razon))
+                                {
+                                    Console.Write("Aviso!:Campo vacio. Debe especificar una razon para el ajuste: "); 
+                                    Console.ReadLine(); 
+                                        
+                                }  
+                        break;
+                    case 5:
+                        inventario.ajusteNegativoDeInventario();
+                        if (opcion == 5)
+                        {
+                           Console.Write("Especifique la razon del ajuste: ");
+                           razon = Console.ReadLine();                             
+                        }                        
+                             if (string.IsNullOrEmpty(razon))
+                            {
+                                Console.Write("Aviso!:Campo vacio. Debe especificar una razon para el ajuste: "); 
+                                Console.ReadLine(); 
+                                       
+                            }                       
+                    break;
+                   
+                    default:
+                     break;
+                }        
+
+                if (opcion == 0)
+                {
+                    break;
+                }    
+            }
+        }
+    }
+}
